844e65a95e53f1c8ce932a5c4a7a54daefd8fa4f
yogeshdp <pyogesh092@gmail.com>
2016-08-13 17:43:27 +0530

    web view demo added

---
+++ WebViewDemo/app/src/main/java/com/example/yogeshpatil/webviewdemo/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.yogeshpatil.webviewdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView webView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = (WebView) findViewById(R.id.webView);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("http://www.google.com");
+    }
+
+
+}
